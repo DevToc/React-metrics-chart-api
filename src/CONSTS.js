ed7985ae67cea4901c11a2b88c9128f5601bdc60
omer shiran <omer shiran>
2022-11-27 16:08:40 +0200

    start convertDataToGraphFormat

---
+++ src/CONSTS.js
@@ -0,0 +1,5 @@
+const METRICS_DOMAIM="http://44.212.66.177"
+const GET_ALL_METRICS_PASS=METRICS_DOMAIM+"/metrics/all"
+
+
+export default {GET_ALL_METRICS_PASS}
--- src/components/DurationButton.jsx
+++ src/components/DurationButton.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import CONSTS from "../CONSTS";
 
 const DurationButton = () => {
+  const [Values,setValues]=useState()
+  const setSpinner=()=>{
+  }
+  const getMetricsAll=async()=>{
+    setSpinner()
+    const response = await fetch(CONSTS.GET_ALL_METRICS_PASS);
+    const myJson = await response.json();
+    const values_list=convertDataToGraphFormat(myJson)
+    setValues(values_list)
+    console.log(values_list);
+  }
+
+
+  const convertDataToGraphFormat=(jsonData)=>{
+    let values_list=[]
+    for (const key in jsonData) {
+    values_list.push({"date":key,"value":parseFloat(jsonData[key].toFixed(4))})
+    }
+    return values_list
+
+  }
   return (
     <div className="flex max-w-[559px] flex-wrap my-6 xl:my-0 mx-auto w-full items-center justify-between ">
-      <button className=" btn-style px-6 py-1">ALL</button>
+      <button className=" btn-style px-6 py-1" onClick={getMetricsAll}>ALL</button>
       <button className="sm:mx-8 mx-2 btn-style px-6 py-1">30 DAYS</button>
       <button className=" btn-style px-6 py-1">24 HOURS</button>
     </div>
