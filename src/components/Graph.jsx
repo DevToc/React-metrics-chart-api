4beb1e115c3068bc6dbd8bb9d693987e59055c63
SecretDevStar <ztoc0130@gmail.com>
2022-11-29 00:15:41 +0900

    indicator3

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -22,7 +22,7 @@ const Graph = ({ range }) => {
     let max = 0;
     for (const key in jsonData) {
       values_list.push({
-        day: new Date(key).toLocaleString(),
+        day: "1-1",
         value: parseFloat(jsonData[key].toFixed(4)),
       });
       if (jsonData[key] < min) {
